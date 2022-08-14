d672d5e47a6f5d3963c216af428e5149ea604168
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-14 16:35:36 +0530

    implemented theme in styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme} from './Theme'
 
+const Container = styled.div`
+background-color: ${({ theme }) => theme.bg};
+color: ${({ theme }) => theme.text};
+`;
 
 function App() {
+  const [darkMode ,setDarkMOde] = useState(true);
   return (
-    <div className="app">
-      <h1>Styled Components</h1>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container className="app">
+        <h1>Styled Components</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Theme.js
@@ -0,0 +1,8 @@
+export const darkTheme = {
+    bg: "Black",
+    text: "white"
+}
+export const lightTheme = {
+    bg: "White",
+    text: "Black"
+}
