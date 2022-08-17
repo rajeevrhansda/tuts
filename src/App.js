bafcc12503f66439722c08c519171e4ef545abdd
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-17 17:32:14 +0530

    get state from redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useSelector } from 'react-redux'
+
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <div className="app">
+      <input
+        placeholder={user.name}
+      />
+      <input
+        placeholder={user.email}
+      />
+      <h2>name:{user.name}</h2>
+      <h2>email:{user.email}</h2>
     </div>
   );
 }
