d774a7e8f7dbefba94905cc75038c17115b62857
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-08-17 17:39:00 +0530

    update redux state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useSelector } from 'react-redux'
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import {update} from './redux/userSlice'
 
 function App() {
   const user = useSelector((state) => state.user);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch()
+  const handleUpdate = (e)=>{
+    e.preventDefault();
+    dispatch(update({name, email}));
+  }
   return (
     <div className="app">
       <input
         placeholder={user.name}
+        onChange={(e)=> setName(e.target.value)}
+        value={name}
       />
       <input
         placeholder={user.email}
+        onChange={(e)=> setEmail(e.target.value)}
+        value={email}
       />
+      <button onClick={handleUpdate}>Update</button>
       <h2>name:{user.name}</h2>
       <h2>email:{user.email}</h2>
     </div>
